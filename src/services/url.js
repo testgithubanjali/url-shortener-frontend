fb6f1b0bff5200b73e65a36b520e7fe86a58f2bd
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-10 17:04:27 +0530

    update: updated the code

--- src/services/url.js
+++ src/services/url.js
@@ -4,3 +4,13 @@ export const shortenURL = async (data) => {
   const response = await api.post("/shorten", data);
   return response.data;
 };
+
+export const getURLs = async () => {
+  const response = await api.get("/urls");
+  return response.data;
+};
+
+export const deleteURL = async (id) => {
+  const response = await api.delete(`/urls/${id}`);
+  return response.data;
+};
